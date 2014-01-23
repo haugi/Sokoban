c09a7f681a5f6890ef762df12b16b79d249103a1
Markus Haug <oxbridon@gmail.com>
2014-01-23 11:43:02 +0100

    External CSS
    Move css to external file

--- index.php
+++ index.php
@@ -7,50 +7,10 @@
 ?>
 <head>
 	<meta charset="UTF-8">
-	<title>Adventure</title>
+	<title>Sokoban</title>
 	
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<style>
-		#screen {
-			width: 400px;
-			height: 400px;
-			border: 1px solid black;
-			margin-left: auto;
-			margin-right: auto;
-			overflow: hidden;
-		}
-		
-		.grid {
-			background-color: #CCCCCC;
-			width: 20px;
-			height: 20px;
-			float: left;
-		}
-		
-		.grey {
-			background-color: #AAAAAA;
-		}
-		
-		.wall {
-			background-color: #000000;
-		}
-		
-		.target {
-			background-color: red;
-		}
-		
-		.box {
-			background-color: yellow;
-		}
-		
-		.player {
-			background-color: green;
-		}
-		
-		.clear {
-			clear: both;
-		}
-	</style>
+	<link rel="stylesheet" href="css/styles.css" type="text/css" media="all"/>
 	
 </head>
 
